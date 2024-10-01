23ce8069de4b0347d946b35363333cfb5ece7bdb
Vincent Bianciotto <vincent@celavi.fr>
2024-10-01 11:52:07 +0200

    Fix inverted links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ function Footer() {
     <footer className="flex flex-col items-center justify-center py-16">
       <p className="text-base text-gray-500">
         Celavi © 2024 Vincent Bianciotto.{' '}
-        <a href="https://github.com/vinzcelavi/celav2-fr/blob/main/" className="text-white/80 hover:text-white transition-all duration-300">
+        <a href="https://github.com/vinzcelavi/celav2-fr/blob/main/mit.md" className="text-white/80 hover:text-white transition-all duration-300">
           MIT licence
         </a>
         .{' '}
-        <a href="https://github.com/vinzcelavi/celav2-fr/blob/main/mit.md" className="text-white/80 hover:text-white transition-all duration-300">
+        <a href="https://github.com/vinzcelavi/celav2-fr/blob/main/" className="text-white/80 hover:text-white transition-all duration-300">
           Source code
         </a>
         .
