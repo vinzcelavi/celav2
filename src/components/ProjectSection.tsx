f3487f70d23e2754a14627d2c47a6e4126418708
Vincent Bianciotto <vincent@celavi.fr>
2024-10-11 07:32:23 +0200

    Autoplay video only when visible

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { cn } from '../utils/cn';
 import AppIcon from './AppIcon';
 import LazyImage from './LazyImage';
+import Video from './Video';
 import Paragraph from './Paragraph';
 
 interface ProjectSectionProps {
@@ -64,7 +65,7 @@ function ProjectSection({ title, subTitle, paragraphs, technos, medias }: Projec
               )}
             >
               {'video' in media ? (
-                <video src={media.video} className="w-full" autoPlay loop muted />
+                <Video src={media.video} />
               ) : (
                 <Suspense fallback={<div>Loading image...</div>}>
                   <LazyImage src={media.img} alt={media.alt} />
---
+++ src/components/Video.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from 'react';
+
+function Video({ src }: { src: string }) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 } // Trigger when 10% of the video is visible
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      if (isVisible) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  }, [isVisible]);
+
+  return <video ref={videoRef} src={src} loop muted playsInline />;
+}
+
+export default Video;
