3e175959d3d23c510bf26acf22e1408b83dc6375
Vincent Bianciotto <vincent@celavi.fr>
2024-10-11 07:18:03 +0200

    Improve code

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -1,27 +1,28 @@
 import { useEffect, useRef, useState } from 'react';
 
-// Lazy image loader component
-const LazyImage = ({ src, alt }: { src: string; alt: string }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const imgRef = useRef<HTMLImageElement | null>(null);
+interface LazyImageProps {
+  src: string;
+  alt: string;
+}
 
-  // Function to trigger when image is fully loaded
+const LazyImage: React.FC<LazyImageProps> = ({ src, alt }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
+
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
 
-  // Check if the image is already cached
   useEffect(() => {
     if (imgRef.current?.complete) {
-      // If the image is already cached, mark it as loaded
       setImageLoaded(true);
     }
   }, []);
 
   return (
-    <div className={`lazy-image ${imageLoaded ? 'loaded' : 'blurred'}`}>
-      <img ref={imgRef} src={src} alt={alt} onLoad={handleImageLoad} className="image" />
-    </div>
+    <figure className={`lazy-image ${imageLoaded ? 'loaded' : 'blurred'}`}>
+      <img ref={imgRef} src={src} alt={alt} onLoad={handleImageLoad} loading="lazy" className="image" />
+    </figure>
   );
 };
 
