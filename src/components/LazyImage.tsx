02395d92c8464f89a26901201889bc982675cf6b
Vincent Bianciotto <vincent@celavi.fr>
2024-10-13 17:14:58 +0200

    Load image only if visible

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -10,17 +10,37 @@ interface LazyImageProps {
 }
 
 const LazyImage: React.FC<LazyImageProps> = ({ src, placeholder, alt, width, height }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
   const imgRef = useRef<HTMLImageElement>(null);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleImageLoad = () => {
-    setImageLoaded(true);
+    if (imgRef.current) {
+      setImageLoaded(true);
+    }
   };
 
   useEffect(() => {
-    if (imgRef.current?.complete) {
-      setImageLoaded(true);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 } // Trigger when 10% of the video is visible
+    );
+
+    if (imgRef.current) {
+      observer.observe(imgRef.current);
+
+      if (imgRef.current.complete) {
+        setImageLoaded(true);
+      }
     }
+
+    return () => {
+      if (imgRef.current) {
+        observer.unobserve(imgRef.current);
+      }
+    };
   }, []);
 
   return (
@@ -42,7 +62,7 @@ const LazyImage: React.FC<LazyImageProps> = ({ src, placeholder, alt, width, hei
         alt={alt}
         width={width}
         height={height}
-        onLoad={handleImageLoad}
+        onLoad={isVisible ? handleImageLoad : undefined}
         className="relative z-20 image"
       />
     </figure>
