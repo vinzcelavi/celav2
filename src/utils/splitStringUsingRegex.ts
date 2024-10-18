d98f40d3f1091b3587491caae56f87878ea5fc2e
Vincent Bianciotto <vincent@celavi.fr>
2024-10-18 12:27:49 +0200

    Use Emoji directly in HelloThere description

--- src/utils/splitStringUsingRegex.ts
+++ src/utils/splitStringUsingRegex.ts
@@ -1,39 +1,13 @@
 function splitStringUsingRegex(inputString: string): string[] {
   const characters: string[] = [];
-  const regex = /(Axeptio|Swile|Teads)([\s\S])/gu;
+  const regex = /[\s\S]/gu;
   let match: RegExpExecArray | null;
 
-  let lastIndex = 0;
   while (true) {
     match = regex.exec(inputString);
     if (match === null) break;
-    // Add characters between matches
-    characters.push(...inputString.slice(lastIndex, match.index).split(''));
-
-    // Add the keyword with HTML
-    const keyword = match[1];
-    switch (keyword) {
-      case 'Axeptio':
-        characters.push(`${keyword} ✋🏻`);
-        break;
-      case 'Swile':
-        characters.push(`${keyword} 💳`);
-        break;
-      case 'Teads':
-        characters.push(`${keyword} 📺`);
-        break;
-      default:
-        characters.push(keyword);
-    }
-
-    // Add the character after the keyword
-    characters.push(match[2]);
-
-    lastIndex = regex.lastIndex;
+    characters.push(match[0]);
   }
-
-  // Add any remaining characters after the last match
-  characters.push(...inputString.slice(lastIndex).split(''));
 
   return characters;
 }
